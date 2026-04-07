46e028589e0539d0625d213f47d0ae97557da7b3
Artem <mailforallaccs@gmail.com>
2026-04-07 14:50:31 +0900

    feat: created shellcode generator

--- src/dialogs/shellcodegeneratordialog.h
+++ src/dialogs/shellcodegeneratordialog.h
@@ -2,16 +2,51 @@
 #define SHELLCODEGENERATORDIALOG_H
 
 #include <QDialog>
-
-class QComboBox;
+#include <QTextEdit>
+#include <QComboBox>
+#include <QPushButton>
+#include <QLabel>
+#include <vector>
+#include <cstdint>
 
 class ShellcodeGeneratorDialog : public QDialog {
-  Q_OBJECT
+    Q_OBJECT
 public:
-  explicit ShellcodeGeneratorDialog(QWidget *parent = nullptr);
+    explicit ShellcodeGeneratorDialog(QWidget* parent = nullptr);
+
+private slots:
+    void onAssemble();
+    void onCopyOutput();
+    void onClear();
 
 private:
-  QComboBox *m_langStyle = nullptr;
+    struct DisasmEntry {
+        int     offset;
+        int     size;
+        QString mnemonic;
+        DisasmEntry(int o, int s, const QString& m) : offset(o), size(s), mnemonic(m) {}
+    };
+
+    QList<DisasmEntry> disassemble(const QByteArray& raw, const QString& bits) const;
+
+    static QString formatAnnotated(const QByteArray& raw, const QList<DisasmEntry>& entries);
+
+    QString generateC(const QByteArray& raw, const QString& bits) const;
+    QString generateCpp(const QByteArray& raw, const QString& bits) const;
+    QString generateRaw(const QByteArray& raw) const;
+
+    void setStatus(const QString& msg, bool error = false);
+
+    static QString findTool(const QString& name);
+
+    QTextEdit* m_asmInput = nullptr;
+    QTextEdit* m_shellcodeOutput = nullptr;
+    QComboBox* m_shellcodeStyle = nullptr;
+    QComboBox* m_archCombo = nullptr;
+    QPushButton* m_copyBtn = nullptr;
+    QPushButton* m_clearBtn = nullptr;
+    QLabel* m_statusLabel = nullptr;
+    QLabel* m_byteCountLabel = nullptr;
 };
 
 #endif // SHELLCODEGENERATORDIALOG_H
